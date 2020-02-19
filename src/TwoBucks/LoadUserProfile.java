dba48c1a9c7e8b8e6623187904048112a8c5b77f
Luis Gonzalez <gg0768@wayne.edu>
2020-02-19 12:39:02 -0500

    Changed loadUser method, so it wouldn't print 'user not found' multiple times

--- src/TwoBucks/LoadUserProfile.java
+++ src/TwoBucks/LoadUserProfile.java
@@ -28,27 +28,31 @@ public class LoadUserProfile {
         String[] fullName = input.split(" ",2);
 
         //search array list for user
-        for (int i = 0; i < users.size(); i++ ) {
+        int i = 0;
+        while(true){
+
+            //if user not found
+            if(i == users.size()){
+                System.out.println("No profile with name "+ fullName[0]+" "+fullName[1]);
+                //flag = false;
+                break;
+            }
+
             check = users.get(i).split(", ",5);
 
             //if user found load user
             if(check[0].equals(fullName[0]) && check[1].equals(fullName[1]) ){
                 System.out.println("Welcome "+fullName[0]+" "+fullName[1]);
                 loadUser = new User(check[0],check[1],check[2],Double.parseDouble(check[3]),Double.parseDouble(check[4]));
+                break;
             }
-            //else user not found
-            else
-                System.out.println("No profile with name"+ fullName[0]+" "+fullName[1]);
+
+
+            i++;
 
         }
-
-
-        System.out.println(loadUser.toString());
-
+        
         return loadUser;
-
-
-
 
     }
 }
