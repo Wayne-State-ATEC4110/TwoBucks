f257792335b43c0f36ff137f214da7c8594d1f43
Frino Jais <el6583@wayne.edu>
2020-02-20 15:09:12 -0500

    Updated without main function

--- src/TwoBucks/EnterFinancialGoals.java
+++ src/TwoBucks/EnterFinancialGoals.java
@@ -12,43 +12,39 @@ public class EnterFinancialGoals {
 
     private static DecimalFormat df = new DecimalFormat("0.00");
 
-    public static float readFloat(Scanner reader) {
-        float num;
-        while (true) {
-            try {
-                num = Float.parseFloat(reader.nextLine());
-                return num;
-            } catch (Exception e) {
-                System.out.println("Please enter a numerical value.");
-            }
-        }
-    }
-
-    public static void main(String args[]) {
-
+    public static void enterGoals() {
         float saveAmt;
         float spendAmt;
 
         Scanner in = new Scanner(System.in);
 
         System.out.println("How much would you like to spend per week?");
-
-        spendAmt = readFloat(in);
-
-        while (spendAmt < 0) {
-            System.out.println("Please enter a value of 0 or higher.");
-
-            spendAmt = readFloat(in);
+        while (true) {
+            try {
+                spendAmt = Float.parseFloat(in.nextLine());
+                if (spendAmt < 0) {
+                    System.out.println("Please enter a value of 0 or higher.");
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a numerical value.");
+            }
         }
         System.out.println("Spending amount of $" + df.format(spendAmt) + " has been saved to your user profile!");
-        System.out.println(" ");
-        System.out.println("How much would you like to save per week?");
 
-        saveAmt = readFloat(in);
-
-        while (saveAmt < 0) {
-            System.out.println("Please enter a value of 0 or higher.");
-            saveAmt = readFloat(in);
+        System.out.println("\nHow much would you like to save per week?");
+        while (true) {
+            try {
+                saveAmt = Float.parseFloat(in.nextLine());
+                if (saveAmt < 0) {
+                    System.out.println("Please enter a value of 0 or higher.");
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a numerical value.");
+            }
         }
         System.out.println("Saving amount of $" + df.format(saveAmt) + " has been saved to your user profile!");
     }
