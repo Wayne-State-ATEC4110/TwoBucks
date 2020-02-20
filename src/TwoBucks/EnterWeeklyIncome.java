c9e74c82ef4da2cee652f460b6f4a86a8e126e7c
Frino Jais <el6583@wayne.edu>
2020-02-20 15:10:01 -0500

    Updated without main function

--- src/TwoBucks/EnterWeeklyIncome.java
+++ src/TwoBucks/EnterWeeklyIncome.java
@@ -12,33 +12,26 @@ public class EnterWeeklyIncome {
 
     private static DecimalFormat df = new DecimalFormat("0.00");
 
-    public static float readFloat(Scanner reader) {
-        float num;
-        while (true) {
-            try {
-                num = Float.parseFloat(reader.nextLine());
-                return num;
-            } catch (Exception e) {
-                System.out.println("Please enter a numerical value.");
-            }
-        }
-    }
-
-    public static void main(String args[]) {
+    public static void enterIncome() {
 
         float weeklyIncome;
 
         Scanner in = new Scanner(System.in);
 
         System.out.println("What is you weekly income?");
-
-        weeklyIncome = readFloat(in);
-
-        while (weeklyIncome < 0) {
-            System.out.println("Please enter a value of 0 or higher.");
-
-            weeklyIncome = readFloat(in);
+        while (true) {
+            try {
+                weeklyIncome = Float.parseFloat(in.nextLine());
+                if (weeklyIncome < 0) {
+                    System.out.println("Please enter a value of 0 or higher.");
+                } else {
+                    break;
+                }
+            } catch (Exception e) {
+                System.out.println("Please enter a numerical value.");
+            }
         }
+
         System.out.println("Weekly income of $" + df.format(weeklyIncome) + " has been saved to your user profile!");
     }
 }
