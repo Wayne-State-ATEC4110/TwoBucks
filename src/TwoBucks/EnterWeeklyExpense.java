178d1cf4b0429b0f3978902db7f0df8cba7f65cb
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2020-03-21 02:51:51 -0400

    Old files were duplicated

--- src/TwoBucks/EnterWeeklyExpense.java
+++ src/TwoBucks/EnterWeeklyExpense.java
@@ -52,35 +52,3 @@ public class EnterWeeklyExpense
         return userExpenses;
     }
 }
-
-package TwoBucks;
-
-// Two Bucks
-// US9. Enter Weekly Expense
-// By Sawyer Kisha
-
-import java.util.Scanner;
-
-public class EnterWeeklyExpense
-{
-    public double setUserExpenses()
-    {
-        //Inputting the weekly expense
-        Scanner scanner = new Scanner(System.in);
-        double userExpenses;
-        System.out.println("Enter your weekly expenses: ");
-        userExpenses = scanner.nextDouble();
-
-        //If input is less than 0 prompt for a new input
-        while(userExpenses < 0)
-        {
-            System.out.println("Invalid: expense must be more than 0 ");
-            System.out.println("Enter your weekly expenses: ");
-            userExpenses = scanner.nextDouble();
-        }
-        //Sends to user
-        //setExpenses(userExpenses);
-        System.out.println("Weekly expense of $" + userExpenses + " has been saved to your user profile!");
-        return userExpenses;
-    }
-}
