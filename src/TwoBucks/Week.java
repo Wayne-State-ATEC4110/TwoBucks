9339e4b828c6a247826aaff3d570e7dd00ad2da8
Luis Gonzalez <gg0768@wayne.edu>
2020-04-13 13:21:16 -0400

    Week updated to work as a nested class for User

--- src/TwoBucks/Week.java
+++ src/TwoBucks/Week.java
@@ -16,13 +16,71 @@ import java.util.Scanner;
 
 public class Week {
 
+     private double income;
+     private double expenses;
+     private double saveAmount;
+     private double spendAmount;
+     private Budget budget;
 
-    private User previousWeek = new User(null,null,null,0,0,0,0,new Budget());
+
 
 
     public Week(){
+        this.income = 0;
+        this.expenses = 0;
+        this.saveAmount = 0;
+        this.spendAmount = 0;
+        this.budget = new Budget();
 
+    }
 
+    public Week(double income, double expenses, double saveAmount,
+                double spendAmount, Budget budget) {
+
+        this.income = income;
+        this.expenses = expenses;
+        this.saveAmount = saveAmount;
+        this.spendAmount = spendAmount;
+        this.budget = budget;    }
+
+    public double getIncome() {
+        return income;
+    }
+
+    public void setIncome(double income) {
+        this.income = income;
+    }
+
+    public double getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(double expenses) {
+        this.expenses = expenses;
+    }
+
+    public double getSaveAmount() {
+        return saveAmount;
+    }
+
+    public void setSaveAmount(double saveAmount) {
+        this.saveAmount = saveAmount;
+    }
+
+    public double getSpendAmount() {
+        return spendAmount;
+    }
+
+    public void setSpendAmount(double spendAmount) {
+        this.spendAmount = spendAmount;
+    }
+
+    public Budget getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Budget budget) {
+        this.budget = budget;
     }
 
     // progress to next week copies user variables and saves them to
@@ -45,25 +103,26 @@ public class Week {
 
             if(input == 1) {
                 // Copy all current user parameters from 'currentWeek' to 'previousWeek'
-                previousWeek.setFirstName(currentWeek.getFirstName());          // first name
-                previousWeek.setLastName(currentWeek.getLastName());            // last name
-                previousWeek.setEmail(currentWeek.getEmail());                  // email
-                previousWeek.setIncome(currentWeek.getIncome());                // income
-                previousWeek.setSaveAmount(currentWeek.getSaveAmount());        // saveAmount
-                previousWeek.setSpendAmount(currentWeek.getSpendAmount());      // spendAmount
+                //previousWeek.setFirstName(currentWeek.getFirstName());          // first name
+                //previousWeek.setLastName(currentWeek.getLastName());            // last name
+                //previousWeek.setEmail(currentWeek.getEmail());                  // email
+                this.setIncome(currentWeek.getIncome());                // income
+                this.setExpenses(currentWeek.getExpenses());
+                this.setSaveAmount(currentWeek.getSaveAmount());        // saveAmount
+                this.setSpendAmount(currentWeek.getSpendAmount());      // spendAmount
 
                 // copy budget parameters
-                previousWeek.getBudget().setTotalIncome(currentWeek.getBudget().getTotalIncome());
-                previousWeek.getBudget().setWageIncome(currentWeek.getBudget().getWageIncome());
-                previousWeek.getBudget().setOtherIncome(currentWeek.getBudget().getOtherIncome());
-                previousWeek.getBudget().setRentExpenses(currentWeek.getBudget().getRentExpenses());
-                previousWeek.getBudget().setUtilitiesExpenses(currentWeek.getBudget().getUtilitiesExpenses());
-                previousWeek.getBudget().setFoodExpenses(currentWeek.getBudget().getFoodExpenses());
-                previousWeek.getBudget().setTravelExpenses(currentWeek.getBudget().getTravelExpenses());
-                previousWeek.getBudget().setHealthcareExpenses(currentWeek.getBudget().getHealthcareExpenses());
-                previousWeek.getBudget().setEntertainmentExpenses(currentWeek.getBudget().getEntertainmentExpenses());
-                previousWeek.getBudget().setTotalExpenses(currentWeek.getBudget().getTotalExpenses());
-                previousWeek.getBudget().setMonthlyNetChange(currentWeek.getBudget().getMonthlyNetChange());
+                this.getBudget().setTotalIncome(currentWeek.getBudget().getTotalIncome());
+                this.getBudget().setWageIncome(currentWeek.getBudget().getWageIncome());
+                this.getBudget().setOtherIncome(currentWeek.getBudget().getOtherIncome());
+                this.getBudget().setRentExpenses(currentWeek.getBudget().getRentExpenses());
+                this.getBudget().setUtilitiesExpenses(currentWeek.getBudget().getUtilitiesExpenses());
+                this.getBudget().setFoodExpenses(currentWeek.getBudget().getFoodExpenses());
+                this.getBudget().setTravelExpenses(currentWeek.getBudget().getTravelExpenses());
+                this.getBudget().setHealthcareExpenses(currentWeek.getBudget().getHealthcareExpenses());
+                this.getBudget().setEntertainmentExpenses(currentWeek.getBudget().getEntertainmentExpenses());
+                this.getBudget().setTotalExpenses(currentWeek.getBudget().getTotalExpenses());
+                this.getBudget().setMonthlyNetChange(currentWeek.getBudget().getMonthlyNetChange());
 
 
                 currentWeek.clearBudget();
@@ -83,30 +142,13 @@ public class Week {
     }
 
 
-
-    /**
-     * previousWeek setter method
-     *
-     * @param previousWeek
-     */
-    public void setPreviousWeek(User previousWeek) {
-        this.previousWeek = previousWeek;
-
+    @Override
+    public String toString() {
+        return this.income +
+                ", " + this.expenses +
+                ", " + this.saveAmount +
+                ", " + this.spendAmount +
+                ", " + this.getBudget().toString()
+                ;
     }
-
-    /**
-     * previousWeek getter method
-     *
-     * @return previousWeek
-     */
-    public User getPreviousWeek() {
-        return this.previousWeek;
-    }
-
-    //@Override
-    //public String toString() {
-     //   return "Week{" +
-     //           "previousWeek=" + previousWeek.toString() +
-     //           '}';
-    //}
 }
