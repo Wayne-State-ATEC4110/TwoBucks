3abbc128b93e73d8b321dd2a10d54ab7e2442c2a
Luis Gonzalez <gg0768@wayne.edu>
2020-04-15 14:35:55 -0400

    Display Goals modified for new implementation

--- Tests/TwoBucks/DisplayGoalsTest.java
+++ Tests/TwoBucks/DisplayGoalsTest.java
@@ -1,17 +1,30 @@
 package TwoBucks;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DisplayGoalsTest {
 
-    @Test
-    void displayGoals() {
-        User user = new User("Bob","Marley", "bob@rasta.com", 1200, 120);
-        user.setBudget(new Budget(10, 20, 30, 40, 50, 60, 70,80));
+    User user;
+
+    /**
+     * Sets up user class for testing each method
+     */
+    @BeforeEach
+    void setup(){
+        user = new User("Bob","Marley", "bob@rasta.com", 1200, 120);
+        user.setBudget(new Budget(10, 20, 30, 40, 50, 60,
+                70,80,90,100,110));
         user.setSaveAmount(100);
         user.setSpendAmount(280);
+        user.setInitialWeek(new Week(500, 200, 100,100,new Budget()));
+        user.setPreviousWeek(new Week(500, 200, 100,100,new Budget()));
+    }
+
+    @Test
+    void displayGoals() {
         DisplayGoals disp = new DisplayGoals();
         disp.displayGoals(user);
 
