68af818bdc6e2f5e75fe1f519275623a40c55ee8
SawyerJKisha <60083349+SawyerJKisha@users.noreply.github.com>
2020-04-20 18:35:21 -0400

    Fix functionality

--- src/TwoBucks/BudgetReminder.java
+++ src/TwoBucks/BudgetReminder.java
@@ -15,8 +15,9 @@ package TwoBucks;
 
 public class BudgetReminder
 {
-    double spendGoal;
 
+    EnterFinancialGoals enterFinancialGoals = new EnterFinancialGoals();
+    double spendGoal = enterFinancialGoals.enterSpendGoal();
     /**
      *  Takes the spendAmt from EnterFinancialGoals.java
      *  and the amount added to the budget in Budget.java
